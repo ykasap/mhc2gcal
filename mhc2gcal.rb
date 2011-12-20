66d33c4470569eb889b7150ccb03b46f71247c38
Munechika Sumikawa <sumikawa@sumikawa.jp>
2011-12-20 18:00:25 +0900

    switch based API to google-api-client

--- mhc2gcal.rb
+++ mhc2gcal.rb
@@ -4,6 +4,7 @@
 ## mhc2gcal
 ## Copyright (C) 2007 Nao Kawanishi <river2470@gmail.com>
 ## Author: Nao Kawanishi <river2470@gmail.com>
+## Copyright (C) 2011 Munechika Sumikawa <sumikawa@sumikawa.jp>
 
 ## "mhc2ical" is base code of mhc2gcal
 ## Original author: Yojiro UO <yuo@iijlab.net>
@@ -15,7 +16,7 @@
 
 require 'yaml'
 require 'rubygems'
-require 'googlecalendar/calendar'
+require 'google/api_client'
 require 'date'
 require 'mhc-schedule'
 require 'mhc-kconv'
@@ -116,8 +117,9 @@ def string_to_date2(s1, s2)
   return item
 end
 
-MHC2GCAL_VERSION = '0.4.0'
-gcal_yaml = File.expand_path('gcal.yaml', File.dirname($0))
+MHC2GCAL_VERSION = '0.5.0'
+
+oauth_yaml = YAML.load_file(File.expand_path('~/.google-api.yaml',  File.dirname($0)))
 date_from   = date_to = MhcDate .new
 category   = '!Holiday'
 secret     = 'Private'
@@ -172,43 +174,39 @@ if secret
   secrets = secret .split .collect{|x| x .downcase}
 end
 
-load_error=false
-File.open( gcal_yaml ) do |io|
-  YAML.load_documents(io){|y|
-    defined? y['gcal_feed'] ? GCAL_FEED=y['gcal_feed'] : load_error=true
-    defined? y['gcal_mail'] ? GCAL_MAIL=y['gcal_mail'] : load_error=true
-    defined? y['gcal_pass'] ? GCAL_PASS=y['gcal_pass'] : load_error=true
-    defined? y['gcal_mode'] ? GCAL_MODE=y['gcal_mode'] : load_error=true
-  }
-end
-if GCAL_MODE == 'delete'
-  GCAL_DEL=true
-elsif GCAL_MODE == 'keep'
-  GCAL_DEL=false
-else
-  load_error = true
-end
-if load_error == true
-  p "YAML loading error..."
-  exit
+client = Google::APIClient.new
+client.authorization.client_id = oauth_yaml["client_id"]
+client.authorization.client_secret = oauth_yaml["client_secret"]
+client.authorization.scope = oauth_yaml["scope"]
+client.authorization.refresh_token = oauth_yaml["refresh_token"]
+client.authorization.access_token = oauth_yaml["access_token"]
+if client.authorization.refresh_token && client.authorization.expired?
+  client.authorization.fetch_access_token!
 end
 
-if proxy_mode
-  GoogleCalendar::Service.proxy_addr=proxy_addr
-  GoogleCalendar::Service.proxy_port=proxy_port
-  if proxy_auth
-    GoogleCalendar::Service.proxy_user=proxy_user
-    GoogleCalendar::Service.proxy_pass=proxy_pass
-    STDOUT .print "Connect to Google Calendar through proxy(#{proxy_user}:#{proxy_pass}@#{proxy_addr}:#{proxy_port})\n"
-  else
-    STDOUT .print "Connect to Google Calendar through proxy(#{proxy_addr}:#{proxy_port})\n"
-  end
+if oauth_yaml["gcal_mode"] == 'delete'
+  GCAL_DEL = true
 else
-  STDOUT .print "Connect to Google Calendar directly\n"
+  GCAL_DEL = false
 end
-srv = GoogleCalendar::Service.new(GCAL_MAIL, GCAL_PASS)
-cal = GoogleCalendar::Calendar.new(srv, GCAL_FEED)
-STDOUT .print "Connect as #{GCAL_MAIL}\n"
+
+# if proxy_mode
+#   GoogleCalendar::Service.proxy_addr=proxy_addr
+#   GoogleCalendar::Service.proxy_port=proxy_port
+#   if proxy_auth
+#     GoogleCalendar::Service.proxy_user=proxy_user
+#     GoogleCalendar::Service.proxy_pass=proxy_pass
+#     STDOUT .print "Connect to Google Calendar through proxy(#{proxy_user}:#{proxy_pass}@#{proxy_addr}:#{proxy_port})\n"
+#   else
+#     STDOUT .print "Connect to Google Calendar through proxy(#{proxy_addr}:#{proxy_port})\n"
+#   end
+# else
+#   STDOUT .print "Connect to Google Calendar directly\n"
+# end
+
+srv = client.discovered_api('calendar', 'v3')
+
+STDOUT .print "Connected\n"
 
 # init arrays for EVENTs in Google Calendar and MHC
 gcal_gevs=[]
@@ -216,14 +214,35 @@ mhc_gevs=[]
 
 # collect EVENTs from Google Calendarin the period of date
 STDOUT .print "Collect EVENTs from Google Calendar\n"
-st = Time.mktime(date_from.y.to_i, date_from.m.to_i, date_from.d.to_i, 0, 0, 0)
-en = Time.mktime(date_to.y.to_i, date_to.m.to_i, date_to.d.to_i, 23, 59, 59)
-cal.events(:'start-min' => st,
-           :'start-max' => en,
-           :'max-results' => 100).each do |event|
-  if event.allday != true or event.en != Time.mktime(date_from.y.to_i, date_from.m.to_i, date_from.d.to_i)
-    gcal_gevs.push(event)
+stg_date = date_from.dec(90)
+stgcal = Time.mktime(stg_date.y.to_i, stg_date.m.to_i, stg_date.d.to_i, 0, 0, 0).gmtime.xmlschema
+st = Time.mktime(date_from.y.to_i, date_from.m.to_i, date_from.d.to_i, 0, 0, 0).gmtime.xmlschema
+en = Time.mktime(date_to.y.to_i, date_to.m.to_i, date_to.d.to_i, 23, 59, 59).gmtime.xmlschema
+
+page_token = nil
+result = client.execute(:api_method => srv.events.list,
+                        :parameters => {'calendarId' => oauth_yaml["calender_id"],
+                          'maxResults' => '100',
+                          'timeZone' => 'Japan/Tokyo',
+                          'timeMin' => stgcal,
+                          'timeMax' => en})
+while true
+  events = result.data.items
+  events.each do |event|
+    if event['start.date'] != nil or event['end'] != Time.mktime(date_from.y.to_i, date_from.m.to_i, date_from.d.to_i).gmtime.xmlschema # xxx: What's this?
+      gcal_gevs.push(event)
+    end
   end
+  if !(page_token = result.data.next_page_token)
+    break
+  end
+  result = client.execute(:api_method => srv.events.list,
+                          :parameters => {'calendarId' => oauth_yaml["calender_id"],
+                            'maxResults' => '100',
+                            'timeZone' => 'Japan/Tokyo',
+                            'timeMin' => stgcal,
+                            'timeMax' => en,
+                            'pageToken' => page_token})
 end
 
 # collect EVENTs from MHC in the period of date
@@ -231,7 +250,6 @@ STDOUT .print "Collect EVENTs from MHC\n"
 db = MhcScheduleDB .new
 db .search(date_from, date_to, category) .each{|date, mevs|
   mevs .each {|mev|
-    gev = cal.create_event
     secret_event = false
     secrets.each{|secret_category|
       regexp = Regexp.new(secret_category, nil, "e")
@@ -241,32 +259,32 @@ db .search(date_from, date_to, category) .each{|date, mevs|
       end
     }
     if secret_event == true
-      gev.title = "SECRET"
+      title = "SECRET"
     else
-      gev.title = NKF.nkf("-w", mev.subject)
+      title = NKF.nkf("-w", mev.subject)
     end
     if mev.location and mev.location != ""
-      gev.where =  NKF.nkf("-w", mev.location)
+      where =  NKF.nkf("-w", mev.location)
     else
-      gev.where =  ""
+      where =  ""
     end
     if mev.time_b.to_s != ""
-      gev.st = Time.parse(date.y.to_s + "/" + date.m.to_s + "/" + date.d.to_s + " " + mev.time_b.to_s)
+      st = Time.parse(date.y.to_s + "/" + date.m.to_s + "/" + date.d.to_s + " " + mev.time_b.to_s).gmtime.xmlschema
       if mev.time_e.to_s != ""
         if mev.time_e.to_i < 86400
-          gev.en = Time.parse(date.y.to_s + "/" + date.m.to_s + "/" + date.d.to_s + " " + mev.time_e.to_s)
+          en = Time.parse(date.y.to_s + "/" + date.m.to_s + "/" + date.d.to_s + " " + mev.time_e.to_s).gmtime.xmlschema
         else
-          gev.en = Time.parse(date.y.to_s + "/" + date.m.to_s + "/" + date.d.to_s + " 23:59")
+          en = Time.parse(date.y.to_s + "/" + date.m.to_s + "/" + date.d.to_s + " 23:59").gmtime.xmlschema
         end
       else
-        gev.en = Time.parse(date.y.to_s + "/" + date.m.to_s + "/" + date.d.to_s + " " + mev.time_b.to_s)
+        en = Time.parse(date.y.to_s + "/" + date.m.to_s + "/" + date.d.to_s + " " + mev.time_b.to_s).gmtime.xmlschema
       end
     else
       allday_start = Date::new(date.y.to_i, date.m.to_i, date.d.to_i)
       allday_end = allday_start + 1
-      gev.st = Time.parse(allday_start.to_s)
-      gev.en = Time.parse(allday_end.to_s)
-      gev.allday = true
+      st = allday_start.to_s
+      en = allday_end.to_s
+      allday = true
     end
     if description == true
       headers = "Category: " + mev .category_as_string + "\n"
@@ -281,9 +299,22 @@ db .search(date_from, date_to, category) .each{|date, mevs|
           switch = false
         end
       }
-      gev.desc = NKF.nkf("-w", headers + "\n" + mev .description .to_s)
+      desc = NKF.nkf("-w", headers + "\n" + mev .description .to_s)
     end
-    mhc_gevs.push(gev)
+
+    event = {
+      'summary' => title,
+      'location' => where,
+      'description' => desc
+    }
+    if allday
+      event['start'] = { 'date' => st }
+      event['end'] = { 'date' => en }
+    else
+      event['start'] = { 'dateTime' => st }
+      event['end'] = { 'dateTime' => en }
+    end
+    mhc_gevs.push(event)
     if verbose == true
     end
   }
@@ -296,18 +327,22 @@ mhc_gevs.uniq
 gcal_gevs.each{|gcal_gev|
   find_the_same_event = false
   mhc_gevs.each{|mhc_gev|
-    if mhc_gev.title == gcal_gev.title &&
-        mhc_gev.where == gcal_gev.where &&
-        mhc_gev.st == gcal_gev.st &&
-        mhc_gev.en == gcal_gev.en &&
-        mhc_gev.desc == gcal_gev.desc
+    if mhc_gev['summary'] == gcal_gev['summary'] &&
+        mhc_gev['location'] == gcal_gev['location'] &&
+        mhc_gev['start.date'] == gcal_gev['start.date'] &&
+        mhc_gev['end.date'] == gcal_gev['end.date'] &&
+        mhc_gev['start.dateTime'] == gcal_gev['start.dateTime'] &&
+        mhc_gev['end.dateTime'] == gcal_gev['end.dateTime'] &&
+        mhc_gev['description'] == gcal_gev['description']
       find_the_same_event = true
       break
     end
   }
   if find_the_same_event != true
     if GCAL_DEL
-      gcal_gev.destroy!
+      result = client.execute(:api_method => srv.events.delete,
+                              :parameters => {'calendarId' => oauth_yaml["calender_id"],
+                                'eventId' => gcal_gev['id']})
     end
     if verbose == true
       if GCAL_DEL
@@ -315,9 +350,9 @@ gcal_gevs.each{|gcal_gev|
       else
         STDOUT .print "Keep EVENT only in Google Calendar\n"
       end
-      STDOUT .print "  What: #{gcal_gev.title}\n"
-      STDOUT .print "  When: #{gcal_gev.st.to_s} - #{gcal_gev.en.to_s}\n"
-      STDOUT .print "  Where: #{gcal_gev.where}\n"
+      STDOUT .print "  What: #{gcal_gev['summary']}\n"
+      STDOUT .print "  When: #{gcal_gev['start']} - #{gcal_gev['end']}\n"
+      STDOUT .print "  Where: #{gcal_gev['location']}\n"
     end
   end
 }
@@ -326,22 +361,27 @@ gcal_gevs.each{|gcal_gev|
 mhc_gevs.each{|mhc_gev|
   find_the_same_event = false
   gcal_gevs.each{|gcal_gev|
-    if mhc_gev.title == gcal_gev.title &&
-        mhc_gev.where == gcal_gev.where &&
-        mhc_gev.st == gcal_gev.st &&
-        mhc_gev.en == gcal_gev.en &&
-        mhc_gev.desc == gcal_gev.desc
+    if mhc_gev['summary'] == gcal_gev['summary'] &&
+        mhc_gev['location'] == gcal_gev['location'] &&
+        mhc_gev['start.date'] == gcal_gev['start.date'] &&
+        mhc_gev['end.date'] == gcal_gev['end.date'] &&
+        mhc_gev['start.dateTime'] == gcal_gev['start.dateTime'] &&
+        mhc_gev['end.dateTime'] == gcal_gev['end.dateTime'] &&
+        mhc_gev['description'] == gcal_gev['description']
       find_the_same_event = true
       break
     end
   }
   if find_the_same_event != true
-    mhc_gev.save!
+    result = client.execute(:api_method => srv.events.insert,
+                            :parameters => {'calendarId' => oauth_yaml["calender_id"]},
+                            :body => JSON.dump(mhc_gev),
+                            :headers => {'Content-Type' => 'application/json'})
     if verbose == true
       STDOUT .print "Create EVENT only in MHC\n"
-      STDOUT .print "  What: #{mhc_gev.title}\n"
-      STDOUT .print "  When: #{mhc_gev.st.to_s} - #{mhc_gev.en.to_s}\n"
-      STDOUT .print "  Where: #{mhc_gev.where}\n"
+      STDOUT .print "  What: #{mhc_gev['summary']}\n"
+      STDOUT .print "  When: #{mhc_gev['start']} - #{mhc_gev['end']}\n"
+      STDOUT .print "  Where: #{mhc_gev['location']}\n"
     end
   end
 }
@@ -349,6 +389,7 @@ mhc_gevs.each{|mhc_gev|
 ### Copyright Notice:
 
 ## Copyright (C) 2007 Nao Kawanishi <river2470@gmail.com>. All rights reserved.
+## Copyright (C) 2011 Munechika Sumikawa <sumikawa@sumikawa.jp>. All rights reserved.
 
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions
